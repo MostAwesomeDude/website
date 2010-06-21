b40e315bb68280e506bd0ba9c7a963d38427c0c7
Corbin Simpson <csimpson@nulllenny.dreamhost.com>
2010-06-20 21:30:33 -0700

    Make things work on DH.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,9 @@ from __future__ import with_statement
 
 import glob
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 import bottle
 
@@ -49,4 +52,4 @@ def index():
     d["entries"].sort(key=lambda x: x["mtime"])
     return d
 
-bottle.run()
+application = bottle.app()
